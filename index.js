2396f00c54f50a8980fd785dcaa237f22fb65a9c
Peyton <1209942pl@gmail.com>
2023-10-23 12:56:52 -0400

    commit message

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  
+  let message;
+if (ride <= 400) {
+    message = 'This one is on me!';
+  }
+if (ride >= 400 && ride <= 2000) {
+  message = 'That will be twenty bucks.';
+}
+if (ride > 2000) {
+  message = 'I will gladly take your thirty bucks.';
+}
+if (ride > 2500) {
+  message = 'No can do.';
+}
+return (message)
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let message;
+  city === "NYC" ? (message = "Ok, sounds good.") : (message = "No go.");
+return (message)  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+let message;
+switch (tip) {
+case 'generous':
+message = "Thank you so much."
+break;
+case 'not as generous':
+message = "Thank you."
+break;
+default:
+message = "Bye.";
+}
+return (message)
 }
